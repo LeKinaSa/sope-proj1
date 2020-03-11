f262377002e18299a8560a21f0aa0dfc0d7c7721
Goncalo Pascoal <goncalojpascoal@gmail.com>
2020-03-11 12:18:53 +0000

    Added restrictions for the number of command line arguments

--- src/main.c
+++ src/main.c
@@ -1,5 +1,12 @@
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 int main(int argc, char* argv[]) {
+    if (argc < 2 || argc > 10) {
+        printf("Usage: %s -l [path] [-a] [-b] [-B size] [-L] [-S] [--max-depth=N]\n", argv[0]);
+        exit(1);
+    }
+
     return 0;
 }
